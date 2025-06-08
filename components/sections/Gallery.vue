ddb14d0c28f49bde246ca0044f119becb67f39dd
Yoki Jati Perkasa <yokijati@gmail.com>
2025-06-08 22:12:58 +0700

    Save

--- components/sections/Gallery.vue
+++ components/sections/Gallery.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
-    
+  <!-- Parent Container -->
+  <div class="min-h-screen">
+    <!-- Container -->
+    <section class="br min-h-screen">
+      <!-- Content -->
+      <div class="by flex flex-col items-start justify-center">
+        <h1 class="text-3xl font-serif text-primary-white">THE GALLERY</h1>
+        <p class="text-sm font-sans text-primary-white">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+          officiis
+        </p>
+        <p class="text-sm font-sans text-primary-white">
+          Photo by Ghani & Wiwit
+        </p>
+      </div>
+    </section>
   </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style>
-
-</style>
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,12 @@
 <template>
   <div class="">
-    <!-- Hero Section -->
-    <section class="">
-      <Hero />
-    </section>
-
-    <!-- Bride Section -->
-    <section class="">
-      <TheBride />
-    </section>
-
-    <!-- Groom Section -->
-    <section class="">
-      <TheGroom />
-    </section>
-
-    <!-- Our Story Section -->
-    <section class="">
-      <OurStory />
-    </section>
-
-    <!-- Countdown Section -->
-    <section class="n">
-      <Countdown />
-    </section>
-
-    <!-- Event Section -->
-    <section class="">
-      <Event />
-    </section>
+    <Hero />
+    <TheBride />
+    <TheGroom />
+    <OurStory />
+    <Countdown />
+    <Event />
+    <Gallery />
   </div>
 </template>
 
@@ -39,22 +17,10 @@ import TheGroom from "~/components/sections/TheGroom.vue";
 import OurStory from "~/components/sections/OurStory.vue";
 import Countdown from "~/components/sections/Countdown.vue";
 import Event from "~/components/sections/Event.vue";
+import Gallery from "~/components/sections/Gallery.vue";
 </script>
 
-<style scoped>
-.scroll-container {
-  height: 100vh;
-  overflow-y: scroll;
-  scroll-snap-type: y mandatory;
-  scroll-behavior: smooth;
-}
-
-.snap-section {
-  height: 100vh;
-  scroll-snap-align: center;
-  scroll-snap-stop: always;
-}
-
+<style>
 .br {
   border: 1px solid red;
 }
